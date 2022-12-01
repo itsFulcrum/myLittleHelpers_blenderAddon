7d8774230c6f45ac32fefcd8f85270b140cc0679
fulcrum <janis.civan@gmail.com>
2022-12-01 19:07:45 +0100

    updatedRenameScript

--- Scripts/BlenderRenameSelection.py
+++ Scripts/BlenderRenameSelection.py
@@ -1,5 +1,9 @@
 import bpy
+# This Script allows you to Change the names of all selected objects at onec or add pre/suffixes to them
 
+#set your custom prefix and suffix here or disable them
+addPrefix = True;
+addSuffix = True;
 prefix = "pre_"
 suffix = "_suff"
 
@@ -7,26 +11,29 @@ suffix = "_suff"
 changeName = True
 newName = "defaultCube"
 
-# Set this true if you want to add versioning at the end like this _0._1
+# Set "addVersioning" True if you want to add versioning at the end with this syntax -> "defaultCube_0" , "defaultCube_1" .. "defaultCube_42"
+#Change "startVersion" if you don't want the versioning to start from 0
 # Keep in mind that blender will automatically add versioning if 2 objects have the same name
 addVersioning = False
 startVersion = 0
 
-selection_list = bpy.context.selected_objects
 
+
+selection_list = bpy.context.selected_objects
 for obj in selection_list:
+    _name = obj.name
 
     if changeName:
-        if addVersioning:
-            obj.name = prefix + newName + suffix + "_" + str(startVersion)
-        else:
-            obj.name = prefix + newName + suffix
-    else:
-        currentName = obj.name
+        _name = newName
+    if addPrefix:
+        _name = prefix + _name
+    if addSuffix:
+        _name = _name + suffix
 
-        if addVersioning:
-            obj.name=prefix + currentName + suffix + "_" + str(sartVersion)
-        else:
-            obj.name = prefix + currentName + suffix
+    if addVersioning:
+        _name = _name + "_" + str(startVersion)
 
-    startVersion = startVersion+1
+
+    obj.name = _name
+
+    startVersion = startVersion + 1
