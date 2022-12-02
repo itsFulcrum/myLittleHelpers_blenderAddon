9cefee7213254e299e15ff23ffc7c523159d2de8
fulcrum <janis.civan@gmail.com>
2022-12-02 16:15:59 +0100

    added functionality to copyTransforms

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 this repository contains a collection of usefull blender scripts I wrote and use to speedup my workflow
 
-all free you can do whatever with it.
+all free you can do whatever with it. Just don't cry if something doesn't work. It's free, use it or not.
+
+
+Addons Directory is still under construction and in development. Nothing finished.
+I really have no idea what I'm doin tbh.
--- Scripts/CopyAllBoneTransformsFromRig.py
+++ Scripts/CopyAllBoneTransformsFromRig.py
@@ -1,27 +1,26 @@
+
 import bpy
 # This Script will add a copy location and copy rotation constraint for each bone of the SourceRig and have it refrence location and rotation from the target rig.
 # For this to work properly it is essentatil that the rigs are identical especially the names have to be the exact same.
 # It is intended to be used if you have two identical rigs and want to copy a pose from one to the other or just reference the animation.
 
-# If you want to apply all of the constraints. well.
-# As of now blender doesn't offer a way to apply all bone constraints on all bones of a rig either through code nor is there a way to apply them through a build in command / shortcut.
-# At least to my knowlage
-
-# The only workaround I know of is to export your rig as a fbx and import it back. The export will apply all constraints automatically. then you can keyframe it.
-
-
 SourceRigName = 'Armature'
 TargetRigName = 'Armature.001'
-# Set "removeAllConstraintsFirst" True if you want all existing Constraints to be removed (Not applyed, Remove) first, before adding new one. Good if you changed something and just want to update it.
-removeAllConstraintsFirst = False
 
-addCopyLocationConstraint = True
+removeAllConstraintsFirst = True # removes all constraints first before adding new ones
+applyAllConstraintsAfter = True # applys all constraints
+
+addCopyLocationConstraint = False
 addCopyRotationConstraint = True
 addCopyScaleConstraint = False
 
+
+# -------------------------------------------------
 source = bpy.data.objects[SourceRigName]
 target = bpy.data.objects[TargetRigName]
 
+userMode = bpy.context.object.mode      #store current mode
+bpy.ops.object.mode_set(mode='POSE')    #set mode to pose mode. Neccesary because we can only use apply operator in pose mode
 
 for bone in source.pose.bones:
 
@@ -30,21 +29,32 @@ for bone in source.pose.bones:
         for constraint in currentConstraints:
             bone.constraints.remove(constraint)
 
+    # set active bone to current bone in loop, to use apply operator on it
+    bpy.context.object.data.bones.active = bone.bone
 
     if addCopyLocationConstraint:
-        bone.constraints.new('COPY_ROTATION')
+        bone.constraints.new('COPY_LOCATION')
         CopyLocationConstraint = bone.constraints[-1]
         CopyLocationConstraint.target = target
         CopyLocationConstraint.subtarget = bone.name
+        if applyAllConstraintsAfter:
+            bpy.ops.constraint.apply(constraint=CopyLocationConstraint.name, owner='BONE')
 
     if addCopyRotationConstraint:
-        bone.constraints.new('COPY_LOCATION')
+        bone.constraints.new('COPY_ROTATION')
         CopyRotationConstraint = bone.constraints[-1]
         CopyRotationConstraint.target = target
         CopyRotationConstraint.subtarget = bone.name
+        if applyAllConstraintsAfter:
+            bpy.ops.constraint.apply(constraint=CopyRotationConstraint.name, owner='BONE')
 
     if addCopyScaleConstraint:
         bone.constraints.new('COPY_SCALE')
         CopyScaleConstraint = bone.constraints[-1]
         CopyScaleConstraint.target = target
         CopyScaleConstraint.subtarget = bone.name
+        if applyAllConstraintsAfter:
+            bpy.ops.constraint.apply(constraint=CopyScaleConstraint.name, owner='BONE')
+
+#set the mode back to what the user had before
+bpy.ops.object.mode_set(mode=userMode)
